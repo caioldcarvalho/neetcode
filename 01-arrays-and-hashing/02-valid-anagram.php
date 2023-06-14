c99c101c4f96449a5ad8907705c41c96c6ea1921
Caio Carvalho <caioldcarvalho@protonmail.com>
2023-06-14 09:16:58 -0300

    create Valid Anagram Problem
    https://leetcode.com/problems/valid-anagram/

---
+++ 01-arrays-and-hashing/02-valid-anagram.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Trying a solution without using "array_reverse()"
+ */
+
+class Solution {
+
+    /**
+     * @param string $s
+     * @param string $t
+     * @return bool
+     */
+    function isAnagram($s, $t) {
+        
+    }
+}
